94f40a7de1e0baaf138ed3200eb183b656013f88
C Schaumans <cs2427@login2.viking2.yor.alces.network>
2024-02-09 15:15:59 +0000

    added a RunTest.py

---
+++ RunTest.py
@@ -0,0 +1,10 @@
+from lunamhd import lunaScan, lunaRead
+
+run = lunaScan(runid = 'test', inputfile = 'default.in')
+run.init_run()
+run.run(scan_saveloc = '/users/cs2427/scratch/lunamhd/KH/test')
+
+read = lunaRead('test', filePath = '/users/cs2427/scratch/lunamhd/KH/test')
+bp = read.basic_plot()
+bp.open_plot()
+bp.save_plot('/users/cs2427/scratch/lunamhd/KH/test/')
