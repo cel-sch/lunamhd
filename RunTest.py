d86f2c484c5d53682b65e06039d4c5ce46967d29
C Schaumans <cs2427@login2.viking2.yor.alces.network>
2024-02-09 15:18:53 +0000

    added save_run to runtest.py

--- RunTest.py
+++ RunTest.py
@@ -3,6 +3,7 @@ from lunamhd import lunaScan, lunaRead
 run = lunaScan(runid = 'test', inputfile = 'default.in')
 run.init_run()
 run.run(scan_saveloc = '/users/cs2427/scratch/lunamhd/KH/test')
+run.save_run()
 
 read = lunaRead('test', filePath = '/users/cs2427/scratch/lunamhd/KH/test')
 bp = read.basic_plot()
