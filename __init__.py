dfcf39c1ccc3531375c03cdb29138f67a2628179
Celine Personal PC <celine.schaumans@york.ac.uk>
2025-01-28 21:10:44 +0000

    real frequency of lunamhd scans set to the negative of themselves (y_vals = -y_vals)

--- __init__.py
+++ __init__.py
@@ -5,9 +5,8 @@ Created on Tue Dec 12 14:25:41 2023
 @author: celin
 """
 from .lunaScanner import lunaScan
-from .lunaScanner_test import lunaScanTest
 from .lunaReader import lunaRead
 from .readh5 import ploth5
 from .plotting import Plotters
 
-__all__ = ["lunaScan", "lunaRead", "ploth5", "lunaScanTest"]
+__all__ = ["lunaScan", "lunaRead", "ploth5"]
